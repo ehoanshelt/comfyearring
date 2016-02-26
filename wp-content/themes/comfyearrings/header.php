5c296151ce13120ad49db7e316bfea903ae75b5f
Eric Hoanshelt <erichoanshelt@Eric-Hoanshelts-MacBook-Pro.local>
2016-02-25 22:27:08 -0600

    Trying offset utility

--- wp-content/themes/comfyearrings/header.php
+++ wp-content/themes/comfyearrings/header.php
@@ -34,4 +34,4 @@
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-	<div id="content" class="site-content nine columns">
+	<div id="content" class="site-content offset-by-three nine columns">
